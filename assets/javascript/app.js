2f9af3d19b264623038aaf72f1a8211958978a83
Nano Bacano <ricardo.bentin@gmail.com>
2018-07-21 17:57:12 -0400

    holy goodness this is crazy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,16 @@
-//what user is searching for in Rovi
-var roviQ;
-var roviAPIKey = "cckmmwwn55kv5qkfy948z5fe";
-var roviSecret = "35e7cff231d0239b8addea47094ba505";
-var sgQ;
-var sgID = "MTIzMDQxNjh8MTUzMTg3OTg5My43Ng";
-var sgSecret = "97e028cca25f9000d81111771fd84a89bef791d0675c8be06d6d50b4547e3096";
+//API KEYS
 var ip;
 var gnQ;
-var gnAPIKey = "pka4bvff8kt2ru6nsny8p3md";
+var gnAPIKey = "mhptv3trpfapay59nwzajpmu";
+var omdbKey = "f7676cd9";
+
 // Other available gnAPIKeys
-    // 1. pka4bvff8kt2ru6nsny8p3md
-    // 2. mhptv3trpfapay59nwzajpmu
-    // 3. vbwkapt65qwb44jjw5vdc98h
+// 1. pka4bvff8kt2ru6nsny8p3md
+// 2. mhptv3trpfapay59nwzajpmu
+// 3. vbwkapt65qwb44jjw5vdc98h
 
 // Gracenote site to request API keys: http://developer.tmsapi.com/apps/mykeys
 var zip;
-
-// $("#roviSubmit").on("click", function () {
-//     event.preventDefault();
-//     roviQ = $("#roviInput").val().trim();
-//     console.log("this is the rovi query: " + roviQ);
-//     var roviQueryURL = `https://api.rovicorp.com/search/v2.1/video/search?entitytype=onetimeonly&query=${roviQ}&rep=1&size=20&offset=0&language=en&country=US&format=json&apikey=${roviAPIKey}&sig=${roviSecret}`;
-//     $.get(roviQueryURL).then(function (roviResponse) {
-//         console.log(roviResponse);
-//     });
-// });
-
-// $("#sgSubmit").on("click", function () {
-//     event.preventDefault();
-//     sgQ = $("#sgInput").val().trim();
-//     console.log("this is the seat geek query: " + sgQ);
-//     var sgQueryURL = `https://api.seatgeek.com/2/events?client_id=${sgID}&client_secret=${sgSecret}&geoip=${ip}`;
-//     $.get(sgQueryURL).then(function (sgResponse) {
-//         console.log(sgResponse);
-//     });
-
-// });
-
 
 //the rest of this document's code is separated between 1. the testing region and 2. the bootstrap template region
 //1. Testing region
@@ -55,18 +29,27 @@ console.log("this is the encoded grace note date", gnDate);
 
 $("#gnMovies").on("click", function () {
     event.preventDefault();
-    var gnMovieQueryURL = `https://data.tmsapi.com/v1.1/movies/showings?startDate=${gnDate}&zip=${zip}&api_key=${gnAPIKey}`;
+    var gnMovieQueryURL = `https://data.tmsapi.com/v1.1/movies/showings?startDate=${gnDate}&zip=19406&api_key=${gnAPIKey}`;
     console.log("This is the grace note Movie Showtime query: " + gnMovieQueryURL);
     $.get(gnMovieQueryURL).then(function (gnMovieResponse) {
         console.log(gnMovieResponse);
         var results = gnMovieResponse;
-        for (var i = 0; i < 4; i++) {
-            $("#apiFeedback").append(`<div>Title: ${results[i].title}<br>Description: ${results[i].shortDescription}<br>Next Showtime: ${results[i].showtimes[i].dateTime}<br>Showing at: ${results[i].showtimes[i].theatre.name}<div>`);
-        }
+        for (var i = 0; i < 5; i++) {
+            $("#apiFeedback").append(`<div>Title: ${results[i].title}<br>Description: ${results[i].shortDescription}<br>Next Showtime: ${results[i].showtimes[0].dateTime}<br>Showing at: ${results[i].showtimes[i].theatre.name}<div>`);
 
-        //notes on movies
-        //need to figure out how to send a radius or even change locale if you don't want it to look up a movie from where you are at that time
-        //need to figure out what the output should look like
+            var omdbQueryURL = `http://www.omdbapi.com/?t=${results[i].title}&apikey=${omdbKey}`;
+
+            $.get(omdbQueryURL).then(function (omdbResponse) {
+                //turn the object into an array
+                var results2 = omdbResponse;
+                console.log("this is the OMBD Response: ", results2);
+                //     //code to do the comparison of the arrays
+                //     // var list = results2.filter(function (val) {
+                //     //     return results.includes(val)
+            });
+            // console.log("THIS IS THE LIST: ", list);
+        };
+
     });
 });
 
@@ -128,16 +111,27 @@ var dateISO = new Date(moment().utcOffset(-8).format("YYYY-MM-DDTHH:mm"));
 var gnDate = encodeURIComponent(dateISO.toISOString().slice(0, 16) + "Z");
 console.log("this is the encoded grace note date", gnDate);
 
-$("#gnMovies").on("click", function () {
-    event.preventDefault();
-    var gnMovieQueryURL = `https://data.tmsapi.com/v1.1/movies/showings?startDate=${gnDate}&zip=${zip}&api_key=${gnAPIKey}`;
-    console.log("This is the grace note Movie Showtime query: " + gnMovieQueryURL);
-    $.get(gnMovieQueryURL).then(function (gnMovieResponse) {
-        console.log(gnMovieResponse);
-        var results = gnMovieResponse;
-        for (var i = 0; i < 4; i++) {
-            $("#apiFeedback").append(`<div>Title: ${results[i].title}<br>Description: ${results[i].shortDescription}<br>Next Showtime: ${results[i].showtimes[i].dateTime}<br>Showing at: ${results[i].showtimes[i].theatre.name}<div>`);
-        }
-    });
-});
+// $("#gnMovies").on("click", function () {
+//     event.preventDefault();
+//     var gnMovieQueryURL = `https://data.tmsapi.com/v1.1/movies/showings?startDate=${gnDate}&zip=${zip}&api_key=${gnAPIKey}`;
+//     console.log("This is the grace note Movie Showtime query: " + gnMovieQueryURL);
+//     $.get(gnMovieQueryURL).then(function (gnMovieResponse) {
+//         console.log(gnMovieResponse);
+//         var results = gnMovieResponse;
+//         for (var i = 0; i < 4; i++) {
+//             $("#apiFeedback").append(`<div>Title: ${results[i].title}<br>Description: ${results[i].shortDescription}<br>Next Showtime: ${results[i].showtimes[i].dateTime}<br>Showing at: ${results[i].showtimes[i].theatre.name}<div>`);
+//         }
+//     });
+// });
 
+// $("#google").on("click", function () {
+//     event.preventDefault();
+//     var proxy = `https://cors-anywhere.herokuapp.com/`;
+//     var google = `http://www.google.com/movies`;
+//     var googleQueryURL = proxy + google;
+//     console.log("THIS IS THE GOOGLE QUERY URL: ",googleQueryURL);
+//     $.get(googleQueryURL).then(function (googleResponse) {
+//         console.log(googleResponse);
+//     });
+
+// });
